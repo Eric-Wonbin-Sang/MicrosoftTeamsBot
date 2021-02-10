bb65f251cc08cb675728c6b355ecbc8afacd9d3f
sang_yoga_laptop <eric.wonbin.sang@gmail.com>
2021-02-10 13:27:16 -0500

    Adding echo bot template

---
+++ main.py
@@ -0,0 +1,12 @@
+
+# pip install botbuilder-core
+# pip install asyncio
+# pip install aiohttp
+# pip install cookiecutter
+
+# https://visualstudio.microsoft.com/visual-cpp-build-tools/
+
+# cookiecutter https://github.com/microsoft/BotBuilder-Samples/releases/download/Templates/echo.zip
+
+# pip install -r requirements.txt
+
